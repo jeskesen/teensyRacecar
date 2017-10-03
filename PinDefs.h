3d56dbf60d6a33d57a9e472350a4a082cac27bee
Justin Eskesen <justin.eskesen@adept.com>
2017-10-03 11:27:54 -0700

    Initial commit.  CPPM passthrough to throttle ESC and steering servo.

---
+++ PinDefs.h
@@ -0,0 +1,18 @@
+/*
+ * PinDefs.h
+ *
+ *  Created on: Oct 3, 2017
+ *  Author: Justin Eskesen
+ */
+
+#ifndef PINDEFS_H_
+#define PINDEFS_H_
+
+const uint8_t CPPM_PIN = 20;
+
+const uint8_t THROTTLE_ESC_PIN = 5;
+const uint8_t STEERING_SERVO_PIN = 6;
+
+
+
+#endif /* PINDEFS_H_ */
---
+++ TeensyRacecar.ino
@@ -0,0 +1,51 @@
+#include "Arduino.h"
+#include "PinDefs.h"
+
+#include <PulsePosition.h>
+
+PulsePositionInput rcIn;
+
+#include <Servo.h>
+Servo steering, throttle;
+float yaw, surge, enable;
+
+void setup(){
+  Serial.begin(115200);
+  rcIn.begin(CPPM_PIN);
+
+  throttle.attach(THROTTLE_ESC_PIN, 1000, 2000);
+  throttle.writeMicroseconds(DEFAULT_PULSE_WIDTH);
+
+  steering.attach(STEERING_SERVO_PIN, 1000, 2000);
+  steering.writeMicroseconds(DEFAULT_PULSE_WIDTH);
+
+}
+
+void loop()
+{
+  yaw=surge=enable=0.0;
+  if(getRCInput(yaw, surge, enable) && (enable > DEFAULT_PULSE_WIDTH + 200))
+  {
+	steering.writeMicroseconds((int)yaw);
+	throttle.writeMicroseconds((int)surge);
+
+    Serial.print(yaw);
+    Serial.print("  ");
+    Serial.print(surge);
+    Serial.println();
+  }
+}
+
+bool getRCInput(float& yaw, float& surge, float& enable)
+{
+  if(rcIn.available() == 6)
+  {
+    yaw = rcIn.read(1); //(rcIn.read(1)-pulseCenter) / 2;
+    surge = rcIn.read(2); //(rcIn.read(2)-pulseCenter) / 2;
+    enable = rcIn.read(5); // > pulseCenter + 200;
+    return true;
+  }
+
+  return false;
+
+}
